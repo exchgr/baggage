1221658dd4609c38a44155f6fe51ce0337af81ac
Dan Mundy <drm.mundy@gmail.com>
2013-03-16 12:28:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baggage::Application.config.secret_token = 'c8d60f7b74a9b64f4950edaf61b274297db2b47b6adb8377d0e40e9e57e2a55db33504d74812fd3717f377c8b206a14b7c4eb71659ff7761c5e7f021811010e2'
