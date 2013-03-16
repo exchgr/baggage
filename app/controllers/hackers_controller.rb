5e6343dcaed5dc75e253b3bd5cc516a7646a977a
Dan Mundy <drm.mundy@gmail.com>
2013-03-16 15:31:56 -0400

    Sets up bootstrap and starts to code hackers/index

--- app/controllers/hackers_controller.rb
+++ app/controllers/hackers_controller.rb
@@ -1,8 +1,17 @@
 class HackersController < ApplicationController
   def index
+    @hackers = Hacker.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
   end
 
   def show
-    @hacker = Hacker.
+  end
+
+  def new
+    @hacker = Hacker.new
   end
 end
